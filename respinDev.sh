6eb879664db31ffeacf0399a773458a4bdef7008
Clover83 <wllmsundberg@gmail.com>
2022-11-29 01:46:22 +0100

    Removed respin files

--- respinDev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker-compose -f docker-compose.yml down -v
-docker-compose -f docker-compose.yml up -d --build
-docker-compose -f docker-compose.yml exec web python manage.py migrate --noinput
